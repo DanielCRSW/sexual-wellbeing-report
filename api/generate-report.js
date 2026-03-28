7e564e8e074058586db5097320f6ca18c78d651a
DanielCRSW <daniel@centrersw.com>
2026-03-28 21:26:01 +1100

    Update generate-report.js

--- api/generate-report.js
+++ api/generate-report.js
@@ -699,7 +699,7 @@ async function sendToPdfMonkey(pdfPayload) {
   return JSON.parse(text);
 }
   
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
