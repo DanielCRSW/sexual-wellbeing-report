c08e703286b6dd94ce317bb6483ba976d34e6a97
DanielCRSW <daniel@centrersw.com>
2026-07-03 13:04:52 +1000

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
             product_data: {
               name: 'Sexual Wellbeing Report',
             },
-            unit_amount: 2900,
+            unit_amount: 3500,
           },
           quantity: 1,
         },
